74d3cf19721dbd8863ab5b45734104fa200121c4
jahskee <Wingman2i>
2019-02-08 12:15:41 -0500

    update 1
    Signed-off-by: jahskee <Wingman2i>

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,3 +1,14 @@
 fun main(args: Array<String>) {
     println("Hello World my name is Jaizon Lubaton!")
+
+    val age = 28
+    println(age)
+
+    var candy = "Snickers";
+    candy = "Fun Dip"
+
+    println(candy)
+
+    var weather = "Sunny"
+    println(weather)
 }
